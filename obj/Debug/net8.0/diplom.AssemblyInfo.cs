85e0a2333d866db0f25e6f5ed8315fc546a93259
sulorat <dima.bessarab@list.ru>
2026-06-01 07:45:17 +0300

    last

--- obj/Debug/net8.0/diplom.AssemblyInfo.cs
+++ obj/Debug/net8.0/diplom.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("diplom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01ba1993aba02b236665d44af2bdd962c24038fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+276267e61f4aacb1debe8cd021c792b4036db280")]
 [assembly: System.Reflection.AssemblyProductAttribute("diplom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("diplom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
